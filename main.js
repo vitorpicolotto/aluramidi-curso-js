94a1de0add952b25fbe62d338bea550110dffa53
vitorpicolotto <picolottovitor@gmail.com>
2023-05-26 16:23:47 -0300

    projeto final do curso!

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){
+   
+        elemento.play()
+  
+    }
+
+    else {
+        alert('Elemento não encontrado');
+
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`
+    const idAudio = `#som_${instrumento}`; //template string
     
     tecla.onclick = function(){
         tocaSom(idAudio);
-    };
+    }
 
-    contador = contador + 1;
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+     
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 
 }
