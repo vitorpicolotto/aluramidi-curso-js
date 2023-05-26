83e8a9e5c665434498ccf75cdf7dadde3b0d2807
vitorpicolotto <picolottovitor@gmail.com>
2023-05-26 11:26:36 -0300

    teclas funcionando a partir do uso do while

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    contador = contador + 1;
+
+}
